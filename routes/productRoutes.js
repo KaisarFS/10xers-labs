44a8f81f7ee354b1bc2b86ecbdc9c3504157a373
Kaisar Firjatullah Sani Setyawan <kaisar.10rpl2.2019@gmail.com>
2024-05-01 23:54:01 +0700

    Fix: unclear message on product controller

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -4,6 +4,8 @@ const authentication = require('../middlewares/authentication');
 const authorization = require('../middlewares/authorization');
 
 router.use(authentication);
+router.get('/', ProductController.getAllProduct);
+router.get('/:id', ProductController.getProductById);
 
 // Admin Routes
 router.use(authorization(['Admin']));
@@ -13,8 +15,6 @@ router.post('/', ProductController.createProduct);
 router.put('/:id', ProductController.updateProduct);
 router.delete('/:id', ProductController.deleteProduct);
 
-router.use(authorization(['Customer']));
-router.get('/', ProductController.getAllProduct);
-router.get('/:id', ProductController.getProductById);
+// router.use(authorization(['Customer']));
 
 module.exports = router;
